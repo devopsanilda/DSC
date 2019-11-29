078df86596f934a4feabd50fd01b1c0993f62ded
Anıl Dalkılıç <57034057+devopsanilda@users.noreply.github.com>
2019-11-29 20:11:27 +0300

    Add files via upload

---
+++ WebserverConfig.ps1
@@ -0,0 +1,16 @@
+﻿Configuration WebserverConfig {
+ 
+    # Import the module for dsc
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+ 
+    Node 'localhost' {
+ 
+        #ensures that the Web-Server (IIS) feature is enabled.
+        WindowsFeature WebServer {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+ 
+     
+    }
+}
